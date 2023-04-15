d8164460e24945ef73d4f9dd83a26750f1f70ebf
Clementino12 <chieque67@gmail.com>
2023-04-15 01:02:18 +0100

    commit

---
+++ JS/projeto-portifolio.js
@@ -0,0 +1,34 @@
+function MenuClick() {
+    var itens = document.querySelector('#itens')
+    itens.classList.toggle('active')
+}
+
+function TypeWriter() {
+    var nome = document.querySelector('h1.nome')
+    let textoArray = nome.innerHTML.split('')
+    nome.innerHTML = ''
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => {
+            nome.innerHTML += letra
+        }, 95 * i)
+    })
+
+}
+
+TypeWriter()
+
+
+
+function nav() {
+    itens.classList.remove('active')
+}
+function mudar() {
+    let body = document.querySelector('body')
+    let header = document.querySelector('.hd-1')
+    let menu = document.querySelector('#itens')
+    body.classList.toggle('mudar')
+    header.classList.toggle('mudar')
+    menu.classList.toggle('mudar')
+    menu.classList.remove('active')
+}
+
